be7a1ff26928a9a662bdbb5b99a5d13201f6a889
thuannd54 <anhthuan666@gmail.com>
2014-07-09 18:07:04 +0700

    Update and rename .gitignore to Beer.java

---
+++ Beer.java
@@ -0,0 +1,21 @@
+
+public class Beer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nguyen Dinh Thuan");
+        System.out.println("09020496");
+        System.out.println("K54BC");
+        System.out.println("thuannd54");
+        System.out.println("thuannd_54@vnu.edu.vn");
+        for(int i=9;i>0;i--){
+            System.out.println(i + " bottles of beer on the wall," + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around."); 
+        }
+        System.out.println("No more bottles of beer on the wall."); 
+        // TODO code application logic here
+    }
+    
+}
